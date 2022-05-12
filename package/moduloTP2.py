3459b4bd6c13e7aa32e938e319c93ecd63458dbd
Sebas <sebas.654@gmail.com>
2022-05-11 23:03:53 -0300

    punto04 terminado

--- package/moduloTP2.py
+++
@@ -1,3 +0,0 @@
-def hola(nombre):
-    msj = 'hola '+nombre+'!!'
-    return msj
--- punto04.py
+++ punto04.py
@@ -3,16 +3,33 @@ from bs4 import BeautifulSoup
 from wordcloud import WordCloud
 from tp02 import modulos as procesoTexto
 import matplotlib.pyplot as plt
+import pandas
+from colorama import Fore, Style, init 
 
+init(autoreset=True)
 
 urlPrincipal = 'https://www.pagina12.com.ar'
 urlBusqueda = 'https://www.pagina12.com.ar/secciones/ciencia'
 
 def obtenerPeticionUrl(url):
+    '''
+    Método que realiza una petición Get para obtener la pagina. 
+    Arg: 
+    url: [str]
+    Return:
+    website: [requests.Response] 
+    '''
     website = requests.get(url)
     return website
 
-def obtenerTitulo(respPeticion):    
+def obtenerTitulo(respPeticion):
+    '''
+        Método que obtiene el contenido de los titulos de esa página. 
+        Arg: 
+            respPeticion: [requests.Response]
+        Return:
+           listaTitulos: [list] 
+    '''   
     if respPeticion.status_code == 200:
         pagina = BeautifulSoup(respPeticion.content, 'html.parser')
     ListaNoticias = pagina(
@@ -24,6 +41,14 @@ def obtenerTitulo(respPeticion):
     return listaTitulos
 
 def obtenerEnlaceSigPagina(respPeticion, urlP=urlPrincipal):
+    '''
+        Método que obtiene el enlace de la siguiente página.
+        Args:
+            respPeticion: [requests.Response]
+            urlP: [str] url principal 
+        Return: 
+            enlace: [list] 
+    '''
     enlace = []
     if respPeticion.status_code == 200:
         pagina = BeautifulSoup(respPeticion.content, 'html.parser')
@@ -35,18 +60,29 @@ def obtenerEnlaceSigPagina(respPeticion, urlP=urlPrincipal):
             enlace.append(e)
     return enlace
 
-def listarNoticias(url, urlP=urlPrincipal):
+def listarNoticias(url, cantidad, urlP=urlPrincipal):
+    '''
+        Método que en base a una direccion web (url) va a obtener una determinada cantidad de
+        títulos.\n
+        Args: 
+            url: [str] url donde buscar.
+            cantidad: [int] cantidad de titulos a obtener.
+            urlP: [str] url prinipal en caso que se tenga un paginador.
+        Return:
+           listaNoticias: [list] lista con los títulos obtenidos. 
+    '''
     cont = 0
     listaNoticias = []
     web = obtenerPeticionUrl(url)
     enlace= ''
-    while cont <= 100:
+    while cont <= cantidad:
         if cont == 0:
+            print(url)
             listaNoticias.extend(obtenerTitulo(web))
             enlace = obtenerEnlaceSigPagina(web)[0]
-            print(enlace)            
+            # print(enlace)            
         else:
-            print('sig pag',enlace)
+            print('sig pag ->',enlace)
             pagina = obtenerPeticionUrl(enlace)
             if pagina.status_code == 200:
                 print(pagina.status_code)
@@ -55,42 +91,51 @@ def listarNoticias(url, urlP=urlPrincipal):
         cont=len(listaNoticias)
     return listaNoticias
 
-# web = obtenerPeticionUrl(urlBusqueda)
-# print(web.status_code)
+def generarNubePalabras(texto):
+    '''
+        Método que genera una nube de palabras usando WordCloud, lo dibuja y lo muestra por pantalla.
+        Arg: 
+            texto: [str] 
+    '''
+    nubePalabra = WordCloud(height=800,width=800, background_color='white', max_words=100, min_font_size=5, collocation_threshold=10).generate(texto)
+    nubePalabra.to_file('nube_palabra.png')
+    # grafico
+    plt.figure(figsize=(10,8))
+    plt.imshow(nubePalabra)
+    plt.axis('off')
+    plt.tight_layout(pad=0)
+    plt.show()
+
+
+# --------------------------------- principal ------------------------------------
 lista100=[]
-listaTotal = listarNoticias(urlBusqueda)
+cantidadNoticias = 100
+listaTotal = listarNoticias(urlBusqueda, cantidadNoticias)  # urlBusqueda = 'https://www.pagina12.com.ar/secciones/ciencia'
 print(len(listaTotal))
-i = 1
 for j in range(100):
-    print(j+1,listaTotal[j])
+    print(j+1,'-',listaTotal[j])
     lista100.append(listaTotal[j])
 
-# unimos todo el texto 
+# unimos todo los títulos en un texto [str] 
 textoTitulos=''
-for t in lista100:    
-    textoTitulos += t +' '
-# print(texto12)
+for titulo in lista100:
+    textoTitulos += titulo +' '
+
 # limpiamos texto signos de puntuacion
 textoSinSignosPuntuacion = procesoTexto.eliminarSignosPuntuacion(textoTitulos)
-# print(textoSinSignosPuntuacion)
+
 # tokenizar texto
 textoTokenizado = procesoTexto.tokenizar(textoSinSignosPuntuacion)
-# print(textoTokenizado)
+
 # poner en minuscula todas las palabras
 textoEnMinuscula = procesoTexto.ponerMinuscula(textoTokenizado)
+
 # eliminiacion stopwords
 textoLimpio = procesoTexto.eliminarStopWords(textoEnMinuscula)
-# print(textoLimpio)
+
 # convertimos en texto
 textoProcesado = ' '.join(textoLimpio)
-print(textoProcesado)
+
 # generar nube de palabras
 print('generando nube de palabras ...')
-nubePalabra = WordCloud(height=800,width=800, background_color='white', max_words=100, min_font_size=5, collocation_threshold=10).generate(textoProcesado)
-nubePalabra.to_file('nube_palabra.png')
-
-plt.figure(figsize=(10,8))
-plt.imshow(nubePalabra)
-plt.axis('off')
-plt.tight_layout(pad=0)
-plt.show()
+generarNubePalabras(textoProcesado)
